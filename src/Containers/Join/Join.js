ab5837f0b6718c1a4e8f891d885edfcf5a9137fc
vkasraj <vikasra1911@gmail.com>
2018-03-07 10:31:53 +0530

    join page styles completed

--- src/Containers/Join/Join.js
+++ src/Containers/Join/Join.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import RandomLy from "../../assets/images/random-ly";
+
 class Join extends Component {
   state = {
     displayName: "",
@@ -25,30 +27,38 @@ class Join extends Component {
 
   render() {
     return (
-      <form>
-        <div className="container">
-          <div className="row">
-            <div className="col-5 mx-auto">
+      <div className="container">
+        <div className="row">
+          <div className="col-11 col-md-6 col-lg-4 mx-auto pt-4">
+            <div className="text-center d-flex justify-content-center align-items-center py-3">
+              <RandomLy width="4rem" />
+              <span className="h3 text-secondary ml-3">random.ly</span>
+            </div>
+            <form onSubmit={this.goToChat}>
               <div className="form-group">
-                <label htmlFor="">Display Name</label>
+                <label htmlFor="" className="text-secondary">
+                  Display Name
+                </label>
                 <input
                   type="text"
-                  className="form-control"
+                  className="form-control badge-pill px-3"
                   placeholder="Display Name"
                   onChange={event =>
                     this.setState({ displayName: event.target.value })
                   }
                   value={this.state.displayName}
                 />
-                <small className="form-text">
+                <small className="form-text text-muted">
                   We will never share your information
                 </small>
               </div>
               <div className="form-group">
-                <label htmlFor="">Room Name</label>
+                <label htmlFor="" className="text-secondary">
+                  Room Name
+                </label>
                 <input
                   type="text"
-                  className="form-control"
+                  className="form-control badge-pill px-3"
                   onChange={event =>
                     this.setState({ roomName: event.target.value })
                   }
@@ -57,16 +67,16 @@ class Join extends Component {
               </div>
               <div className="text-center">
                 <button
-                  className="btn btn-primary w-100"
-                  onClick={this.goToChat}
+                  className="btn w-100 badge-pill text-white font-weight-bold"
+                  style={{ background: "#0fa" }}
                 >
                   Let's Chat
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
-      </form>
+      </div>
     );
   }
 }
