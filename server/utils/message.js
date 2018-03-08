31c849b72d0faaa3f6f35e117c3995132104c92f
vkasraj <vikasra1911@gmail.com>
2018-03-08 13:02:37 +0530

    fixed: react key props duplicate in messageList

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,5 +1,8 @@
+const uuidv4 = require("uuid/v4");
+
 const generateMsg = (from, text) => {
   return {
+    msgId: uuidv4(),
     from,
     text,
     createdAt: new Date()
--- src/Containers/MessagesList/MessagesList.js
+++ src/Containers/MessagesList/MessagesList.js
@@ -6,7 +6,7 @@ import Message from "../../Components/Message/Message";
 class MessagesList extends Component {
   render() {
     const messages = this.props.messagesList.map(msg => (
-      <Message message={msg} key={msg.text} />
+      <Message message={msg} key={msg.msgId} />
     ));
     return messages;
   }
