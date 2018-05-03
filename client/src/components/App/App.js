36d4f79cae3743d5049cc8c5370b0c5dc90f686b
vkasraj <vikasraj1911@gmail.com>
2018-05-03 12:32:06 +0530

    redux-devtools: ENABLED

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from "react";
 import { CssBaseline } from "material-ui";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 // components
 import reducer from "../../Store/reducer/index";
 import Routes from "../Routes/Routes";
 
+// redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const REDUCER = combineReducers(reducer);
 
-const STORE = createStore(REDUCER);
+const STORE = createStore(REDUCER, composeEnhancers(
+    applyMiddleware(/*Add Middleware*/)
+));
 
 const APP = (
     <Provider store={STORE}>
