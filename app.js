bd36761b084d1817ac5dbec6d475268afa7423e1
vkasraj <vikasra1911@gmail.com>
2018-05-06 21:50:24 +0530

    server: fixed CORS error

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
     res.send({
         welcome: 'home'
