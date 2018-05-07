5956c1015f24f8fb12a033f55f762a011bdf3ce8
vkasraj <vikasraj1911@gmail.com>
2018-05-07 15:25:34 +0530

    config: react proxy workaround

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
 
-app.get('/', (req, res) => {
+/*
+    1) React proxy will not work with only root url, which just have '/'.
+    2) Make sure you add specific domain to work with react proxy i.e., '/user/login' or '/user/signup'.
+*/
+
+app.get("/", (req, res) => {
     res.send({
-        welcome: 'home'
-    })
-})
+        welcome: "home"
+    });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is up on port: ${PORT}`)
+    console.log(`Server is up on port: ${PORT}`);
 });
