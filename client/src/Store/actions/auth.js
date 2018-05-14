5aef42afc92f7c37a87e9734f4ed5916722c8fe1
vkasraj <vikasraj1911@gmail.com>
2018-05-14 15:00:41 +0530

    signup: not setting user in store on signup

--- client/src/Store/actions/auth.js
+++ client/src/Store/actions/auth.js
@@ -1,100 +1,104 @@
 import axios from "axios";
 
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    };
+	return {
+		type: actionTypes.AUTH_START
+	};
 };
 
 export const authSuccess = data => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        data
-    };
+	return {
+		type: actionTypes.AUTH_SUCCESS,
+		data
+	};
 };
 
 export const authFail = error => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error
-    };
+	return {
+		type: actionTypes.AUTH_FAIL,
+		error
+	};
 };
 
 export const authLogout = () => {
-    localStorage.removeItem('TOKEN');
-    return {
-        type: actionTypes.AUTH_LOGOUT,
-    };
+	localStorage.removeItem("TOKEN");
+	return {
+		type: actionTypes.AUTH_LOGOUT
+	};
 };
 
 export const signup = (data, callback) => {
-    return dispatch => {
-        dispatch(authStart());
-        axios
-            .post("/auth/signup", data)
-            .then(d => {
-                const D = d.data;
-                if (D.status) {
-                    dispatch(authSuccess(D.user));
-                    callback(D);
-                } else {
-                    dispatch(authFail(D.err));
-                }
-            })
-            .catch(e => {
-                dispatch(authFail(e));
-                throw e;
-            });
-    };
+	return dispatch => {
+		dispatch(authStart());
+		axios
+			.post("/auth/signup", data)
+			.then(d => {
+				const D = d.data;
+				if (D.status) {
+					callback(D);
+				} else {
+					dispatch(authFail(D.err));
+				}
+			})
+			.catch(e => {
+				dispatch(authFail(e));
+				throw e;
+			});
+	};
 };
 
 export const login = (data, callback) => {
-    return dispatch => {
-        dispatch(authStart());
-        axios
-            .post("/auth/login", data)
-            .then(d => {
-                const D = d.data;
-                if (D.status && D.user) {
-                    localStorage.setItem('TOKEN', D.token);
-                    dispatch(authSuccess(D));
-                    callback(D);
-                } else {
-                    dispatch(authFail(D.message));
-                }
-            })
-            .catch(e => {
-                dispatch(authFail(e));
-                throw e;
-            });
-    };
+	return dispatch => {
+		dispatch(authStart());
+		axios
+			.post("/auth/login", data)
+			.then(d => {
+				const D = d.data;
+				if (D.status && D.user) {
+					localStorage.setItem("TOKEN", D.token);
+					dispatch(authSuccess(D));
+					callback(D);
+				} else {
+					dispatch(authFail(D.message));
+				}
+			})
+			.catch(e => {
+				dispatch(authFail(e));
+				throw e;
+			});
+	};
 };
 
-export const authAutoSignIn = (callback) => {
-    return dispatch => {
-        const token = localStorage.getItem('TOKEN');
-        if (!token) {
-            dispatch(authLogout());
-        } else {
-            dispatch(authStart());
-            axios.post('/auth/authenticate', {
-                token: token
-            }).then(d => {
-                const D = d.data;
-                if (D.status) {
-                    dispatch(authSuccess({
-                        user: D.user,
-                        token: token
-                    }))
-                    callback(D);
-                } else {
-                    dispatch(authFail(D.message))
-                }
-            }).catch(error => {
-                dispatch(authFail(error));
-            })
-        }
-    }
-}
+export const authAutoSignIn = callback => {
+	return dispatch => {
+		const token = localStorage.getItem("TOKEN");
+		if (!token) {
+			dispatch(authLogout());
+		} else {
+			dispatch(authStart());
+			axios
+				.post("/auth/authenticate", {
+					token: token
+				})
+				.then(d => {
+					const D = d.data;
+					if (D.status) {
+						dispatch(
+							authSuccess({
+								user: D.user,
+								token: token
+							})
+						);
+						callback(D);
+					} else {
+						dispatch(authFail(D.message));
+					}
+				})
+				.catch(error => {
+					dispatch(authFail(error));
+				});
+		}
+	};
+};
