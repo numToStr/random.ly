0dec06694b297477bd357628f6abb1c7e7dc5306
vkasraj <vikas@klickinvite.com>
2018-05-29 15:51:06 +0530

    styles: fixed scrolling issue on mobile

--- client/src/Containers/Chat/Chat.js
+++ client/src/Containers/Chat/Chat.js
@@ -3,9 +3,9 @@ import { connect } from "react-redux";
 import socketIO from "socket.io-client";
 
 import {
-  setCurrentUser,
-  setUsers,
-  messageRecieved
+	setCurrentUser,
+	setUsers,
+	messageRecieved
 } from "../../store/actions/index";
 
 import Input from "../Input/Input";
@@ -16,145 +16,147 @@ import UsersList from "../UsersList/UsersList";
 const io = socketIO();
 
 class Chat extends Component {
-  state = {
-    connect: null
-  };
+	state = {
+		connect: null
+	};
 
-  componentDidMount() {
-    this.onConnect();
-    this.onNewMessage();
-    this.onUpdateUserList();
-    this.onDisconnect();
-  }
+	componentDidMount() {
+		this.onConnect();
+		this.onNewMessage();
+		this.onUpdateUserList();
+		this.onDisconnect();
+	}
 
-  onConnect = () => {
-    io.on("connect", () => {
-      this.onJoin();
-    });
-  };
+	onConnect = () => {
+		io.on("connect", () => {
+			this.onJoin();
+		});
+	};
 
-  onNewMessage = () => {
-    let msgs = [];
-    io.on("newMessage", msg => {
-      msgs = [...msgs, msg];
-      this.props.onSetMessages(msgs);
-      this.scrollToBottom();
-    });
-  };
+	onNewMessage = () => {
+		let msgs = [];
+		io.on("newMessage", msg => {
+			msgs = [...msgs, msg];
+			this.props.onSetMessages(msgs);
+			this.scrollToBottom();
+		});
+	};
 
-  onUpdateUserList = () => {
-    io.on("updateUserList", users => {
-      this.props.onSetUsers(users);
-    });
-  };
+	onUpdateUserList = () => {
+		io.on("updateUserList", users => {
+			this.props.onSetUsers(users);
+		});
+	};
 
-  onGetCurrentUser = () => {
-    const u = {};
-    for (const key in this.props.currentUser) {
-      const val = this.props.currentUser[key];
-      if (val) {
-        u[key] = val;
-      } else {
-        u[key] = sessionStorage[key];
-      }
-    }
-    this.props.onSetCurrentUser(u);
-  };
+	onGetCurrentUser = () => {
+		const u = {};
+		for (const key in this.props.currentUser) {
+			const val = this.props.currentUser[key];
+			if (val) {
+				u[key] = val;
+			} else {
+				u[key] = sessionStorage[key];
+			}
+		}
+		this.props.onSetCurrentUser(u);
+	};
 
-  onJoin = () => {
-    this.onGetCurrentUser();
+	onJoin = () => {
+		this.onGetCurrentUser();
 
-    io.emit("join", this.props.currentUser, (err, data) => {
-      if (err) {
-        this.props.history.replace("/");
-      } else {
-        sessionStorage.setItem("name", data.currentUser.name);
-        sessionStorage.setItem("room", data.currentUser.room);
-        sessionStorage.setItem("token", data.currentUser.token);
+		io.emit("join", this.props.currentUser, (err, data) => {
+			if (err) {
+				this.props.history.replace("/");
+			} else {
+				sessionStorage.setItem("name", data.currentUser.name);
+				sessionStorage.setItem("room", data.currentUser.room);
+				sessionStorage.setItem("token", data.currentUser.token);
 
-        this.setState({ connect: "User Connected" });
-        // console.log("User Connected", this.props.currentUser);
-      }
-    });
-  };
+				this.setState({ connect: "User Connected" });
+				// console.log("User Connected", this.props.currentUser);
+			}
+		});
+	};
 
-  onDisconnect = () => {
-    io.on("disconnect", () => {
-      this.setState({ connect: "User Disconnected" });
-      // console.log("User Disconnected");
-    });
-  };
+	onDisconnect = () => {
+		io.on("disconnect", () => {
+			this.setState({ connect: "User Disconnected" });
+			// console.log("User Disconnected");
+		});
+	};
 
-  onCreateMessage = (e, msg) => {
-    e.preventDefault();
+	onCreateMessage = (e, msg) => {
+		e.preventDefault();
 
-    io.emit("createMessage", {
-      text: msg
-    });
-  };
+		io.emit("createMessage", {
+			text: msg
+		});
+	};
 
-  scrollToBottom = () => {
-    const messagesContainer = this.messagesListContainer;
-    const newMessage =
-      messagesContainer.children[messagesContainer.children.length - 1];
-    const lastMessage = messagesContainer.children[
-      messagesContainer.children.length - 2
-    ]
-      ? messagesContainer.children[messagesContainer.children.length - 2]
-      : 0;
+	scrollToBottom = () => {
+		const messagesContainer = this.messagesListContainer;
+		const newMessage =
+			messagesContainer.children[messagesContainer.children.length - 1];
+		const lastMessage = messagesContainer.children[
+			messagesContainer.children.length - 2
+		]
+			? messagesContainer.children[messagesContainer.children.length - 2]
+			: 0;
 
-    const clientHeight = messagesContainer.clientHeight;
-    const scrollTop = messagesContainer.scrollTop;
-    const scrollHeight = messagesContainer.scrollHeight;
+		const clientHeight = messagesContainer.clientHeight;
+		const scrollTop = messagesContainer.scrollTop;
+		const scrollHeight = messagesContainer.scrollHeight;
 
-    const newMessageHeight = newMessage.clientHeight;
-    const lastMessageHeight = lastMessage.clientHeight;
+		const newMessageHeight = newMessage.clientHeight;
+		const lastMessageHeight = lastMessage.clientHeight;
 
-    if (
-      clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
-      scrollHeight
-    ) {
-      messagesContainer.scrollTop = scrollHeight;
-    }
-  };
+		if (
+			clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+			scrollHeight
+		) {
+			messagesContainer.scrollTop = scrollHeight;
+		}
+	};
 
-  render() {
-    return (
-      <AuxComp>
-        <div className="container-fluid">
-          <div className="row">
-            <UsersList />
-            <div className="col-12 col-md-8">
-              <div className="d-flex flex-column vh-100">
-                <div
-                  className="flex-grow overflow-auto mt-2"
-                  ref={el => (this.messagesListContainer = el)}
-                >
-                  <MessagesList />
-                </div>
-                {/* <small>{this.state.connect}</small> */}
-                <Input submit={this.onCreateMessage} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </AuxComp>
-    );
-  }
+	render() {
+		return (
+			<AuxComp>
+				<div className="container-fluid h-100">
+					<div className="row h-100">
+						<UsersList />
+						<div className="col-12 col-md-8">
+							<div className="d-flex flex-column h-100">
+								<div
+									className="flex-grow overflow-auto mt-2"
+									ref={el =>
+										(this.messagesListContainer = el)
+									}
+								>
+									<MessagesList />
+								</div>
+								{/* <small>{this.state.connect}</small> */}
+								<Input submit={this.onCreateMessage} />
+							</div>
+						</div>
+					</div>
+				</div>
+			</AuxComp>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    currentUser: state.users.currentUser
-  };
+	return {
+		currentUser: state.users.currentUser
+	};
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    onSetCurrentUser: currentUser => dispatch(setCurrentUser(currentUser)),
-    onSetMessages: messages => dispatch(messageRecieved(messages)),
-    onSetUsers: users => dispatch(setUsers(users))
-  };
+	return {
+		onSetCurrentUser: currentUser => dispatch(setCurrentUser(currentUser)),
+		onSetMessages: messages => dispatch(messageRecieved(messages)),
+		onSetUsers: users => dispatch(setUsers(users))
+	};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chat);
--- client/src/Containers/UsersList/UsersList.js
+++ client/src/Containers/UsersList/UsersList.js
@@ -4,26 +4,26 @@ import { connect } from "react-redux";
 import Users from "../../Components/Users/Users";
 
 class UsersList extends Component {
-  render() {
-    return (
-      <div className="d-none d-md-block col-md-4 px-0">
-        <div className={"bg-light vh-100"}>
-          <p className="display-4 text-center">People</p>
-          <Users
-            usersList={this.props.usersList}
-            currentUser={this.props.currentUser}
-          />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="d-none d-md-block col-md-4 px-0">
+				<div className="bg-light h-100">
+					<p className="display-4 text-center">People</p>
+					<Users
+						usersList={this.props.usersList}
+						currentUser={this.props.currentUser}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    usersList: state.users.users,
-    currentUser: state.users.currentUser
-  };
+	return {
+		usersList: state.users.users,
+		currentUser: state.users.currentUser
+	};
 };
 
 export default connect(mapStateToProps)(UsersList);
