a8e7b59746c9f85a1e19ae7cd730a1b3589186cf
vkasraj <vikasraj1911@gmail.com>
2018-06-04 11:52:57 +0530

    Routes: added loader until component is loaded

--- client/src/Root/Routes/config/AsyncRoutes.js
+++ client/src/Root/Routes/config/AsyncRoutes.js
@@ -1,17 +1,34 @@
+import React from "react";
 import { asyncComponent } from "react-async-component";
+import { Grid, CircularProgress } from "@material-ui/core";
 
+// Rendering Loader until component is loaded ==============
+const Loader = (
+	<Grid container>
+		<Grid item xs={12} className="text-center pt-5">
+			<CircularProgress />
+		</Grid>
+	</Grid>
+);
+
+// Async Routes ===========
 export const Home = asyncComponent({
-	resolve: () => import("../../../containers/Home/Home")
+	resolve: () => import("../../../containers/Home/Home"),
+	LoadingComponent: () => Loader
 });
 export const SignUp = asyncComponent({
-	resolve: () => import("../../../containers/SignUp/SignUp")
+	resolve: () => import("../../../containers/SignUp/SignUp"),
+	LoadingComponent: () => Loader
 });
 export const LogIn = asyncComponent({
-	resolve: () => import("../../../containers/LogIn/LogIn")
+	resolve: () => import("../../../containers/LogIn/LogIn"),
+	LoadingComponent: () => Loader
 });
 export const Chat = asyncComponent({
-	resolve: () => import("../../../containers/Chat/Chat")
+	resolve: () => import("../../../containers/Chat/Chat"),
+	LoadingComponent: () => Loader
 });
 export const Profile = asyncComponent({
-	resolve: () => import("../../../containers/Profile/Profile")
+	resolve: () => import("../../../containers/Profile/Profile"),
+	LoadingComponent: () => Loader
 });
