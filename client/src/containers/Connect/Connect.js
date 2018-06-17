d4e23d4e260d2928d29381f3a43251d8f7f45cbc
vkasraj <vikasraj1911@gmail.com>
2018-06-17 13:06:55 +0530

    Connect: using history.replce instead of <Redirect />

--- client/src/containers/Connect/Connect.js
+++ client/src/containers/Connect/Connect.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import {
-	Typography,
-	Button,
-	FormControl,
-	CircularProgress
+    Typography,
+    Button,
+    FormControl,
+    CircularProgress
 } from "@material-ui/core";
 
 import { authLogout } from "../../Store/actions/index";
@@ -13,91 +12,82 @@ import RoomConnect from "../../components/Forms/RoomConnect/RoomConnect";
 import FormLayout from "../../components/FormLayout/FormLayout";
 
 class Connect extends Component {
-	state = {
-		room: null
-	};
+    joinUser = ({ room, selectedRoom }) => {
+        const {
+            history: { replace }
+        } = this.props;
 
-	joinUser = ({ room, selectedRoom }) => {
-		selectedRoom = selectedRoom === "---" ? null : selectedRoom;
+        selectedRoom = selectedRoom === "---" ? null : selectedRoom;
 
-		let R = "anonymous";
-		if (room && selectedRoom) {
-			R = room;
-		} else if (room || selectedRoom) {
-			R = room || selectedRoom;
-		}
+        let R = "anonymous";
+        if (room && selectedRoom) {
+            R = room;
+        } else if (room || selectedRoom) {
+            R = room || selectedRoom;
+        }
 
-		this.setState({
-			room: R
-		});
-	};
+        replace({
+            pathname: "/chat",
+            search: `?room=${R}`
+        });
+    };
 
-	render() {
-		const { joinUser } = this;
-		const { ioLoading } = this.props;
-		const { room } = this.state;
+    render() {
+        const { joinUser } = this;
+        const { ioLoading } = this.props;
 
-		if (room) {
-			return (
-				<Redirect
-					push
-					to={{ pathname: "/chat", search: `?room=${room}` }}
-				/>
-			);
-		}
-
-		return (
-			<FormLayout>
-				<RoomConnect loading={ioLoading} onSubmit={joinUser} />
-				<FormControl margin="normal" fullWidth>
-					<Typography
-						variant="subheading"
-						color="textSecondary"
-						align="center"
-					>
-						or
-					</Typography>
-				</FormControl>
-				<Typography align="center" paragraph>
-					<Button
-						variant="raised"
-						color="primary"
-						fullWidth
-						onClick={joinUser}
-						disabled={ioLoading}
-					>
-						{ioLoading ? (
-							<CircularProgress
-								size={20}
-								thickness={4}
-								color="secondary"
-							/>
-						) : (
-							"Connect"
-						)}
-					</Button>
-				</Typography>
-				<Typography variant="caption" color="secondary" align="center">
-					--- You will be connected to default room ---
-				</Typography>
-			</FormLayout>
-		);
-	}
+        return (
+            <FormLayout>
+                <RoomConnect loading={ioLoading} onSubmit={joinUser} />
+                <FormControl margin="normal" fullWidth>
+                    <Typography
+                        variant="subheading"
+                        color="textSecondary"
+                        align="center"
+                    >
+                        or
+                    </Typography>
+                </FormControl>
+                <Typography align="center" paragraph>
+                    <Button
+                        variant="raised"
+                        color="primary"
+                        fullWidth
+                        onClick={joinUser}
+                        disabled={ioLoading}
+                    >
+                        {ioLoading ? (
+                            <CircularProgress
+                                size={20}
+                                thickness={4}
+                                color="secondary"
+                            />
+                        ) : (
+                            "Connect"
+                        )}
+                    </Button>
+                </Typography>
+                <Typography variant="caption" color="secondary" align="center">
+                    --- You will be connected to default room ---
+                </Typography>
+            </FormLayout>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-	return {
-		ioLoading: state.io.loading
-	};
+    return {
+        ioLoading: state.io.loading
+    };
 };
 
 const mapDispatchToProps = dispatch => {
-	return {
-		logout: () => dispatch(authLogout())
-	};
+    return {
+        logout: () => dispatch(authLogout())
+    };
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Connect);
