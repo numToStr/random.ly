9d98bd47609896518dcd80072cd8fc0c0157966d
vkasraj <vikasraj1911@gmail.com>
2018-06-18 18:57:25 +0530

    build

---
+++ client/build/static/js/11.52e4cf2e.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([11],{1873:function(e,r,a){"use strict";Object.defineProperty(r,"__esModule",{value:!0});var t=a(1),n=a.n(t),i=a(167),s=function(e){return{drawerPaper:{position:"relative",height:"100% !important",width:"100%"}}},c=function(e){var r=e.classes,a=e.side,t=e.children;return n.a.createElement(i.j,{variant:"permanent",anchor:a,className:"h-100",classes:{paper:r.drawerPaper}},t)};r.default=Object(i.A)(s)(c)}});
+//# sourceMappingURL=11.52e4cf2e.chunk.js.map
