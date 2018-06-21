9a4a376f5461db8171e4f0e13efd71d22381e46a
vkasraj <vikasraj1911@gmail.com>
2018-06-21 14:46:03 +0530

    themeChooser: changing app theme

--- client/src/Root/App/App.js
+++ client/src/Root/App/App.js
@@ -1,81 +1,76 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-
+import { connect } from "react-redux";
 import grey from "@material-ui/core/colors/grey";
 
-// components
-import reducer from "../../Store/reducer/index";
 import Routes from "../Routes/Routes";
 
-// redux devtools
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+class APP extends Component {
+	render() {
+		const { themeColor } = this.props;
 
-const REDUCER = combineReducers(reducer);
-
-const STORE = createStore(
-	REDUCER,
-	composeEnhancers(applyMiddleware(thunk /*Add Middleware*/))
-);
-
-const THEME = createMuiTheme({
-	// shadows: Array(25),
-	overrides: {
-		MuiButton: {
-			root: {
-				borderRadius: "10rem"
+		const THEME = createMuiTheme({
+			// shadows: Array(25),
+			overrides: {
+				MuiButton: {
+					root: {
+						borderRadius: "10rem"
+					},
+					raised: {
+						boxShadow: "none"
+					}
+				},
+				MuiAppBar: {
+					root: {
+						boxShadow: "none"
+					}
+				}
 			},
-			raised: {
-				boxShadow: "none"
+			palette: {
+				primary: {
+					main: themeColor
+				},
+				secondary: {
+					main: grey[700]
+				}
+			},
+			typography: {
+				fontFamily: [
+					// "Roboto Mono",
+					// "monospace",
+					"-apple-system",
+					"BlinkMacSystemFont",
+					'"Segoe UI"',
+					"Roboto",
+					'"Helvetica Neue"',
+					"Arial",
+					"sans-serif",
+					'"Apple Color Emoji"',
+					'"Segoe UI Emoji"',
+					'"Segoe UI Symbol"'
+				].join(",")
 			}
-		},
-		MuiAppBar: {
-			root: {
-				boxShadow: "none"
-			}
-		}
-	},
-	palette: {
-		primary: {
-			main: grey[900]
-		},
-		secondary: {
-			main: grey[700]
-		}
-	},
-	typography: {
-		fontFamily: [
-			// "Roboto Mono",
-			// "monospace",
-			"-apple-system",
-			"BlinkMacSystemFont",
-			'"Segoe UI"',
-			"Roboto",
-			'"Helvetica Neue"',
-			"Arial",
-			"sans-serif",
-			'"Apple Color Emoji"',
-			'"Segoe UI Emoji"',
-			'"Segoe UI Symbol"'
-		].join(",")
+		});
+
+		return (
+			<BrowserRouter>
+				<Fragment>
+					<MuiThemeProvider theme={THEME}>
+						<CssBaseline />
+						<Routes />
+					</MuiThemeProvider>
+				</Fragment>
+			</BrowserRouter>
+		);
 	}
-});
+}
 
-const APP = (
-	<Provider store={STORE}>
-		<BrowserRouter>
-			<Fragment>
-				<MuiThemeProvider theme={THEME}>
-					<CssBaseline />
-					<Routes />
-				</MuiThemeProvider>
-			</Fragment>
-		</BrowserRouter>
-	</Provider>
-);
+const mapStateToProps = state => {
+	return {
+		themeColor: state.theme.color
+	};
+};
 
-export default APP;
+export default connect(mapStateToProps)(APP);
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,30 @@
+import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import "./styles.global.scss";
 import App from "./Root/App/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(App, document.getElementById("root"));
+import reducer from "./Store/reducer/index";
+// redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const REDUCER = combineReducers(reducer);
+
+const STORE = createStore(
+	REDUCER,
+	composeEnhancers(applyMiddleware(thunk /*Add Middleware*/))
+);
+
+ReactDOM.render(
+	<Provider store={STORE}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 registerServiceWorker();
 
 /* 
